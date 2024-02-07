d279c3f51462051b6b495eda7cd13d218ce2e014
Cesar Uriarte <cesarujos@gmail.com>
2024-02-07 18:26:50 -0500

    Corrigiendo filtro por rango de fechas

--- src/services/rd.ts
+++ src/services/rd.ts
@@ -11,17 +11,25 @@ const findDataRD = async (body: {}, query: any) => {
             application_name: "My SuiteCRM REST Client",
             name_value_list: {},
         })
-
+        //OBTENER SECUENCIA SQL PARA FILTRAR EN API
         const filter = Object.entries(body)
             .filter(([_clave, valor]) => valor !== "")
-            .map(([clave, valor]) => `${clave} = "${valor}"`)
+            .map(([clave, valor]) => {
+                if (clave === "active_date") {
+                    const valorAny = valor as any;
+                    // Tratar el rango de fechas como un BETWEEN en SQL
+                    return `${clave} BETWEEN '${valorAny["active_date_inicial"]}' AND '${valorAny["active_date_final"]}'`;
+                } else {
+                    return `${clave} = "${valor}"`;
+                }
+            })
             .join(" AND ");
         const dataRD = await tefiRequest("get_entry_list", {
             session: loginResponseTefi['id'],
             module_name: "RSD_R_directorales",
             query: filter,
             order_by: "",
-            offset: Number(query["page"])*Number(query["max_results"]) || 0,
+            offset: Number(query["page"]) * Number(query["max_results"]) || 0,
             select_fields: ["document_name", "subcategory_id", "director", "administrado", "link_rd_c", "active_date"],
             link_name_to_fields_array: [{
                 "name": "rsd_r_directorales_iga_igas",
